74c5507bd6f6d75bfe7d06e896acc1e596ae4564
Ulises Catarinolo <ulisescatarinolo@outlook.com>
2022-09-21 15:21:43 -0300

    adding gh-pages II

--- src/components/context/ShowMoviesContext.jsx
+++ src/components/context/ShowMoviesContext.jsx
@@ -5,10 +5,11 @@ export const useShowMoviesContext = () => useContext(ShowMoviesContext)
 
 const ShowMoviesProvider = ({ children }) => {
     const [movies, setMovies] = useState([]);
+    const [filterData, setFilterData] = useState([]);
     const [search, setSearch] = useState("");
 
     return (
-        <ShowMoviesContext.Provider value={{ movies, setMovies, search, setSearch }}>
+        <ShowMoviesContext.Provider value={{ movies, setMovies, search, setSearch, filterData, setFilterData }}>
             {children}
         </ShowMoviesContext.Provider>
     );
